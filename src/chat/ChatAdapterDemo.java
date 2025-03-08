27818ce9937e421b6102bff5376e3e53376a741a
Qaraqat-ul <karakat.zhumagali@narxoz.kz>
2025-03-08 15:13:18 +0600

    Обновлён код Singleton и Adapter

--- src/chat/ChatAdapterDemo.java
+++ src/chat/ChatAdapterDemo.java
@@ -1,1 +1,9 @@
- 
+ package chat;
+
+public class ChatAdapterDemo {
+    public static void main(String[] args) {
+        LegacyChatService legacyService = new LegacyChatService();
+        ChatService adapter = new ChatServiceAdapter(legacyService);
+        adapter.sendMessage("Hello world!");
+    }
+}
